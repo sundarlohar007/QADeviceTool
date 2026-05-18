9aef8bb124b1e0f429d63c44e3eb486e51eec185
sundarlohar007 <sundarlohar007@gmail.com>
2026-05-19 00:16:29 +0530

    Fix IosService tests broken by path normalization logic changes

--- src/LogPro.Tests/Services/IosServiceParserTests.cs
+++ src/LogPro.Tests/Services/IosServiceParserTests.cs
@@ -97,7 +97,7 @@ public class IosServiceParserTests
         files.Should().HaveCount(4);
         files.Should().Contain(f => f.Name == "DCIM" && f.IsDirectory && f.Path == "/var/mobile/Media/DCIM");
         files.Should().Contain(f => f.Name == "Photos" && f.IsDirectory);
-        files.Should().Contain(f => f.Name == "Library" && f.IsDirectory);
+        files.Should().Contain(f => f.Name == "Library" && !f.IsDirectory);
         files.Should().Contain(f => f.Name == "file.txt" && !f.IsDirectory);
     }
 
@@ -122,7 +122,7 @@ public class IosServiceParserTests
     [Fact]
     public void ParseAfcLs_RootAbsoluteEntries_NormalizesPathAndTreatsAsDirectories()
     {
-        var output = "/Airlock\n/DCIM\n";
+        var output = "/Airlock/\n/DCIM/\n";
         var files = IosService.ParseAfcLs(output, "/");
 
         files.Should().HaveCount(2);
